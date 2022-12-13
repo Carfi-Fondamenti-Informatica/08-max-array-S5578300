2fb201f460f907e3964015c3bd69a803bdaf1333
S5578300 <118068983+S5578300@users.noreply.github.com>
2022-12-13 01:37:23 +0100

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+int N;
+cin >> N;
+float a[N];
+for (int 1 = 0; 1 < N; i++){
+cin >> a[i];
+}
+cout << massimo( a, N);
+return 0;
 }
